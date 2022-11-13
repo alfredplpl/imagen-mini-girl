c6c79c953e1d89d1ddcc050c96a14fda676ec9ef
Yasunori Ozaki <hackalf80@outlook.jp>
2022-11-13 21:04:16 +0900

    v.0.1

--- generate_images.py
+++ generate_images.py
@@ -4,6 +4,7 @@ import torch
 import pickle
 
 BATCH_SIZE=8
+CUDA=0
 
 with open("prompts_1024.pickle", "rb") as f:
     prompts=pickle.load(f)
@@ -20,13 +21,16 @@ pipe = StableDiffusionPipeline.from_pretrained(
     revision="fp16",
     use_auth_token=os.environ["HUG_KEY"],
     scheduler=euler_scheduler,
-).to("cuda:0")
+).to(f"cuda:{CUDA}")
 
 for seed in range(0, 1024):
     out_dir=os.path.join(os.environ["DS_PATH"], f"seed_{seed:09d}")
     if(not os.path.exists(out_dir)):
         os.mkdir(out_dir)
     for i in range(0,len(prompts),BATCH_SIZE):
+        out_path = os.path.join(out_dir, f"{i:09d}.png")
+        if(os.path.exists(out_path)):
+            continue
         images = pipe(prompts[i:i+BATCH_SIZE], num_inference_steps=50,seed=seed).images
         for j,image in enumerate(images):
             out_path=os.path.join(out_dir, f"{i+j:09d}.png")
