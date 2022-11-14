b1b0f3c86ec36b0df8f2496d8ed2bc2f4b3647fe
Yasunori Ozaki <hackalf80@outlook.jp>
2022-11-14 12:24:49 +0900

    Add larges

--- generate_images.py
+++ generate_images.py
@@ -6,7 +6,7 @@ import pickle
 BATCH_SIZE=8
 CUDA=0
 
-with open("prompts_1024.pickle", "rb") as f:
+with open("prompts/extract_prompts_116067.pickle", "rb") as f:
     prompts=pickle.load(f)
 
 euler_scheduler = EulerDiscreteScheduler.from_config(
@@ -31,7 +31,19 @@ for seed in range(0, 64):
         out_path = os.path.join(out_dir, f"{i:09d}.png")
         if(os.path.exists(out_path)):
             continue
+        [print(x) for x in prompts[i:i+BATCH_SIZE]]
         images = pipe(prompts[i:i+BATCH_SIZE], num_inference_steps=50,seed=seed).images
+        for j,image in enumerate(images):
+            out_path=os.path.join(out_dir, f"{i+j:09d}.png")
+            if (not os.path.exists(out_path)):
+                image.save(out_path)
+
+    # terminal
+    if(len(prompts)!=i):
+        out_path = os.path.join(out_dir, f"{i:09d}.png")
+        if(os.path.exists(out_path)):
+            continue
+        images = pipe(prompts[i:], num_inference_steps=50,seed=seed).images
         for j,image in enumerate(images):
             out_path=os.path.join(out_dir, f"{i+j:09d}.png")
             if (not os.path.exists(out_path)):
--- prompts/README.md
+++ prompts/README.md
@@ -1,3 +1,13 @@
 # Large Prompts
-
+## extract_prompts_116067.pickle
 [extracted prompts](https://1drv.ms/u/s!ApxVlgxlqLRliLop2hrmNvL4f_bHUw?e=Zx8sK7)
+These scripts are a part of DiffusionDB. Thanks.
+```bibtex
+@article{wangDiffusionDBLargescalePrompt2022,
+  title = {{{DiffusionDB}}: {{A}} Large-Scale Prompt Gallery Dataset for Text-to-Image Generative Models},
+  author = {Wang, Zijie J. and Montoya, Evan and Munechika, David and Yang, Haoyang and Hoover, Benjamin and Chau, Duen Horng},
+  year = {2022},
+  journal = {arXiv:2210.14896 [cs]},
+  url = {https://arxiv.org/abs/2210.14896}
+}
+```
